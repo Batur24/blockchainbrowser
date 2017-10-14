75bbe8a4ff260d58a017b032788d315e70abdf4f
Batur <heshupai@gmail.com>
2017-10-14 21:52:43 +0800

    单个区块详细页面

--- pages/blockInfo/blockInfo.js
+++ pages/blockInfo/blockInfo.js
@@ -1,65 +1,33 @@
-// pages/blockInfo/blockInfo.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    height: "",
+    blockInfo: {}
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-  
+    let height = options.height
+    this.getBlockInfo(height)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  getBlockInfo: function(height) {
+    let that = this
+    wx.request({
+      url: "http://10.30.95.156:8080/api/blockchain/get/block/" + height,
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        let data = res.data
+        console.log(data)
+        that.setData({
+          height: height,
+          blockInfo: data
+        })
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage: function () {
   
   }
